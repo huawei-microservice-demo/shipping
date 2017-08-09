ee94abefc20c5d9e2370ab980fbc44055fec6aab
preethikathamuthu <preethi.kathamuthu@huawei.com>
2017-08-09 12:53:44 +0000

    shipping complete migration

--- src/test/java/works/weave/socks/shipping/controllers/ITShippingController.java
+++ src/test/java/works/weave/socks/shipping/controllers/ITShippingController.java
@@ -1,49 +1,43 @@
-package works.weave.socks.shipping.controllers;
+/*package works.weave.socks.shipping.controllers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.*;
 import works.weave.socks.shipping.entities.Shipment;
 import works.weave.socks.shipping.entities.HealthCheck;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.*;
-
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ITShippingController {
-    // @MockBean
+    @MockBean
     private RabbitTemplate rabbitTemplate;
 
-    //@Autowired
+    @Autowired
     private ShippingController shippingController;
 
-    //@Test
+    @Test
     public void getShipment() throws Exception {
         String shipping = shippingController.getShipping();
         assertThat(shipping, is(notNullValue()));
     }
 
-    //  @Test
+    @Test
     public void getShipmentById() throws Exception {
         String shipping = shippingController.getShippingById("id");
         assertThat(shipping, is(notNullValue()));
     }
 
-    //  @Test
+    @Test
     public void newShipment() throws Exception {
         Shipment original = new Shipment("someName");
         Shipment saved = shippingController.postShipping(original);
@@ -51,13 +45,13 @@ public class ITShippingController {
         assertThat(original, is(equalTo(saved)));
     }
 
-    // @Test
+    @Test
     public void getHealthCheck() throws Exception {
         Map<String, List<HealthCheck>> healthChecks = shippingController.getHealth();
         assertThat(healthChecks.get("health").size(), is(equalTo(2)));
     }
 
-    // @Test
+    @Test
     public void doNotCrashWhenNoQueue() throws Exception {
         doThrow(new AmqpException("test error")).when(rabbitTemplate).convertAndSend(anyString(),
                 any(Shipment.class));
@@ -67,3 +61,4 @@ public class ITShippingController {
         assertThat(original, is(equalTo(saved)));
     }
 }
+*/
